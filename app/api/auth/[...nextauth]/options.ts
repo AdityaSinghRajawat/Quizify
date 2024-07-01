e67d1eb9097444ea094512236ff5b1857aa93b9e
Aditya Singh Rajawat <adityasinghrajawat393@gmail.com>
2024-07-02 00:18:05 +0530

    debug in deployment

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -62,6 +62,7 @@ export const options: NextAuthOptions = {
         }
     },
 
-    secret: process.env.NEXTAUTH_SECRET
+    secret: process.env.NEXTAUTH_SECRET,
+    debug: process.env.NODE_ENV === 'development',
 
 };
